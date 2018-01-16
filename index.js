b5569888f4b4844be8128481a0811ad841cd236f
A Lee <ayslee@gmail.com>
2018-01-16 23:25:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
+
+updateObjectWithKeyAndValue(recipes, "prop", 1);
+console.log(recipes);
+
+var recipes = {}
+
+function destructivelyUpateObjectWithKeyAndValue(object, key, value) {
+  
+}
